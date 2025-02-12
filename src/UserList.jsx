69bc66cc5f8966349efc7de59c2d3a7bd7fea60f
Tejas Nambiar <tejasnambiar32@gmail.com>
2025-02-12 12:54:21 +0530

    added userList css

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -1,18 +1,38 @@
 import React from "react";
+import "./styles/UserList.css";
 
 const UserList = ({ users, onUpdateAge, onDeleteUser }) => {
   return (
-    <div>
-      {users.map((user) => (
-        <div key={user.id}>
-          <h1>{user.name}</h1>
-          <h1>{user.age}</h1>
-          <button onClick={() => onUpdateAge(user.id, user.age)}>
-            Update Age
-          </button>
-          <button onClick={() => onDeleteUser(user.id)}>Delete User</button>
-        </div>
-      ))}
+    <div className="user-table-container">
+      <table className="user-table table-bordered">
+        <thead>
+          <tr>
+            <th>Sr.No.</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Country</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody className="user-table-body">
+          {users.map((user, index) => (
+            <tr key={user.id}>
+              <td>{index + 1}</td>
+              <td>{user.name}</td>
+              <td>{user.age}</td>
+              <td>{user.country}</td>
+              <td className="action-buttons">
+                <button onClick={() => onUpdateAge(user.id, user.age)}>
+                  Update Age
+                </button>
+                <button onClick={() => onDeleteUser(user.id)}>
+                  Delete User
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
